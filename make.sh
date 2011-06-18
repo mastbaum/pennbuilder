f7c8d3b96ac51d46169a129180a54ff6d4926a6d
mastbaum <none@none>
2011-06-18 15:50:17 -0400

    whitepaper on event builder

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
-gcc event_builder.c server.c ds.c -o event_builder -ljemalloc -I./include ${1}
+gcc event_builder.c server.c ds.c -o event_builder -ljemalloc -lpthread -I./include ${1}
 gcc client.c -o client -ljemalloc -I./include ${1}
 
