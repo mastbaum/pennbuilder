25b6f18b41602cf23e2a91fb26df6517c96a0b14
Andy Mastbaum <amastbaum@gmail.com>
2012-03-07 23:15:15 -0600

    more improvements to run transitions

--- io/shipper.h
+++ io/shipper.h
@@ -1,8 +1,8 @@
 #ifndef __PENNBUILDER_SHIPPER__
 #define __PENNBUILDER_SHIPPER__
 
-#define QUEUE_DELAY 5.0
-#define SKIP_GTID_DELAY 0.1
+#define QUEUE_DELAY 0.1
+#define SKIP_GTID_DELAY 1.0
 #define DISPATCHER_ADDRESS "tcp://*:5024"
 #define MAX_RHDR_WAIT 100000*50
 
